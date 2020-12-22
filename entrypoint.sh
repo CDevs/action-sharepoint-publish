ef23fb61d072e55b34935ac9d71b967fbd3d2368
CDevs <cozieeddy@gmail.com>
2020-12-22 22:04:31 +0000

    Remove uneeded actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -l
 
 echo "Moving to SharePoint";
-#mkdir /out
-#cd $GITHUB_WORKSPACE
-#zip -r /out/repoarchive.zip ./build
-#export FILE_PATH='/out/repoarchive.zip'
+
 
 node /app/index.js
 [ $? -eq 0 ]  || exit 1
