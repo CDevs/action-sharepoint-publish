7af841a5cc2102a612ac2fca3cad1dab4964c875
CDevs <cozieeddy@gmail.com>
2020-12-22 21:17:41 +0000

    Upates to deploy action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
-echo "Creating archive";
-mkdir /out
-cd $GITHUB_WORKSPACE
-zip -r /out/repoarchive.zip ./build
-export FILE_PATH='/out/repoarchive.zip'
+echo "Moving to SharePoint";
+#mkdir /out
+#cd $GITHUB_WORKSPACE
+#zip -r /out/repoarchive.zip ./build
+#export FILE_PATH='/out/repoarchive.zip'
 
 node /app/index.js
 [ $? -eq 0 ]  || exit 1
